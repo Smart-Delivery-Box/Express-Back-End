ee6df91270aae1c70704e6b20be55e59bf520210
Julio leon <julioleon@Julios-MBP.localdomain>
2022-10-21 08:37:26 -0400

    added routes and queries for finding payment methods by using customer number

--- Routes/customerRoutes.js
+++ Routes/customerRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connection = require('../Connection/connection')
-const findPaymentMethodsByFirstName = require('../Queries/customerQueries')
+const customerQueries = require('../Queries/customerQueries')
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -10,18 +10,16 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/payment-methods/:firstName', (req, res) => {
-    // const firstName = req.params.firstName
-    // connection.query(`SELECT Customer_Number FROM Customers WHERE First_Name = '${firstName}'`, (err, result) => {
-    //     if (err) throw err;
-    //     const customerNumber = result[0].Customer_Number
-    //     connection.query(`SELECT * FROM Customer_Card_Info WHERE Customer_Number = ${customerNumber}`, (err, card) => {
-    //         if (err) throw err;
-    //         res.json(card)
-    //         // paymentMethods = card
-    //     })
-    // })
-    findPaymentMethodsByFirstName(req.params.firstName, (err, data) => {
+router.get('/payment-methods/name/:firstName', (req, res) => {
+    customerQueries.findPaymentMethodsByFirstName(req.params.firstName, (err, data) => {
+        if (err) throw err;
+        else
+            res.json(data)
+    })
+})
+
+router.get('/payment-methods/number/:customerNumber', (req, res) => {
+    customerQueries.findPaymentMethodsByCustomerNumber(req.params.customerNumber, (err, data) => {
         if (err) throw err;
         else
             res.json(data)
